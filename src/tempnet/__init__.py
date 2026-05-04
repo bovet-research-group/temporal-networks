4eec3c399ff0b136fac223696ea7ffb4db812c3d
Jonas I. Liechti <j-i-l@t4d.ch>
2026-05-04 17:24:29 +0200

    Split - This promotes tempnet as independant module (#11)
    * proper hatchling build setup
    
    * minor install adaptations
    
    * moving over more modules
    
    * proper dependency on stochmat
    
    * update dependency
    
    * target correct stochmat
    
    * corrected nameing
    
    * started with ci
    
    * completing testing dependencies
    
    * fixing formatting
    
    * formatting
    
    * formatting
    
    * recover from attr renaming
    
    * update dependency
    
    * minor cleanups
    
    * fix dev workflow
    
    * upgrade dependencies
    
    * fixes in initiations
    
    * assert presence of ending_times
    
    * adapt and clomplete testing
    
    * remove redundancies
    
    * start to deprecate child class
    
    * mino cleanup
    
    * minor
    
    * Revert "start to deprecate child class"
    
    This reverts commit 3845fb421b1c2494ec42478bfb41715c2f7e18ea.
    
    * actually use memray
    
    * minor fix in coverage naming

--- src/tempnet/__init__.py
+++ src/tempnet/__init__.py
@@ -26,12 +26,25 @@ License
 GNU Lesser General Public License v3 or later (LGPLv3+).
 
 """
+try:
+    # try to import version (provided by hatch (see pyproject.toml)
+    from ._version import __version__
+except ImportError:
+    # Fallback if the package wasn't installed properly
+    __version__ = "unknown"
+
 import logging
 
 from .logger import setup_logger, get_logger
 
 # Default log level
 setup_logger()  # Set up the logger with the default level
+
+from .temporal_network import (  # noqa: F401
+    ContTempNetwork,
+    set_to_zeroes,
+    sparse_lapl_expm
+)
 
 def set_log_level(level):
     """
