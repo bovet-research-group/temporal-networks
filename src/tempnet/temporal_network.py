79c23ee47ec9e7dfdc82119fff2492d80cafad38
YasAsgari <yasaman.asgari@math.uzh.ch>
2026-05-13 14:14:56 +0200

    Add the functions first and last of step 5 in #15

--- src/tempnet/temporal_network.py
+++ src/tempnet/temporal_network.py
@@ -2284,6 +2284,35 @@ class ContTempInstNetwork(ContTempNetwork):
             fix_tau_k=True,
             use_sparse_stoch=use_sparse_stoch
         )
+    def active_nodes(self, t_start, t_end):
+        """Return the nodes active within the given time window."""
+        assert t_start < t_end , \
+            "t_end should be bigger than t_start"
+
+        t_start=max(self.start_time, t_start)
+        t_end=min(self.end_time, t_end)    
+        mask = (self.events_table["starting_times"] < t_end) & (self.events_table["ending_times"] > t_start)
+        edges = self.events_table[mask]
+        nodes = set(edges["source_nodes"]).union(set(edges["target_nodes"]))
+        return np.array(list(nodes))
+
+    def num_active_nodes(self, t_start, t_end):
+        """Return the number of nodes active within the given time window."""
+        nodes=active_nodes(self, t_start, t_end)
+        return len(nodes)
+
+
+    def num_active_edges(self, t_start, t_end):
+        """Return the number of edges active within the given time window."""
+        assert t_start < t_end , \
+            "t_end should be bigger than t_start"
+
+        t_start=max(self.start_time, t_start)
+        t_end=min(self.end_time, t_end)   
+        
+        mask = (self.events_table["starting_times"] < t_end) & (self.events_table["ending_times"] > t_start)
+        return mask.sum()
+
 
 
 def lin_approx_trans_matrix(T, t, Pi=None, t_s=10):
@@ -2802,31 +2831,3 @@ def set_to_zeroes(Tcsr, tol=1e-8, relative=True, use_absolute_value=False):
         else:
             raise TypeError("Tcsr must be csc,csr or SparseStochMat")
 
-    def active_nodes(self, t_start, t_end):
-        """Return the nodes active within the given time window."""
-        assert t_start < t_end , \
-            "t_end should be bigger than t_start"
-
-        t_start=max(self.start_time, t_start)
-        t_end=min(self.end_time, t_end)    
-        mask = (self.events_table["starting_times"] < t_end) & (self.events_table["ending_times"] > t_start)
-        edges = self.events_table[mask]
-        nodes = set(edges["source_nodes"]).union(set(edges["target_nodes"]))
-        return np.array(list(nodes))
-
-    def num_active_nodes(self, t_start, t_end):
-        """Return the number of nodes active within the given time window."""
-        nodes=active_nodes(self, t_start, t_end)
-        return len(nodes)
-
-
-    def num_active_edges(self, t_start, t_end):
-        """Return the number of edges active within the given time window."""
-        assert t_start < t_end , \
-            "t_end should be bigger than t_start"
-
-        t_start=max(self.start_time, t_start)
-        t_end=min(self.end_time, t_end)   
-        
-        mask = (self.events_table["starting_times"] < t_end) & (self.events_table["ending_times"] > t_start)
-        return mask.sum()
