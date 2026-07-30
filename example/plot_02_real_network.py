84fc674ceec764894db876963d1150b7f7dabb30
YasAsgari <yasaman.asgari@math.uzh.ch>
2026-07-30 15:21:14 +0200

    fixed the readthedocs issues

--- example/plot_02_real_network.py
+++ example/plot_02_real_network.py
@@ -194,7 +194,7 @@ print("End:", tnet.end_time)
 # Number of active nodes in each one-hour window across a full day.
 
 t = np.arange(0, 24 * 3600 + 1, 3600)
-n_active = [tnet.num_active_nodes(t[i], t[i + 1]) for i in range(len(t) - 1)]
+n_active = [tnet.num_active_nodes(None, t[1])]+[tnet.num_active_nodes(t[i], t[i + 1]) for i in range(1,len(t) - 1)]
 fig, ax = plt.subplots(nrows=1, ncols=1, dpi=200)
 ax.plot(t[:-1], n_active, marker='.')
 
@@ -210,11 +210,10 @@ plt.show()
 # Number of active edges/events in each one-hour window.
 
 t = np.arange(0, 24 * 3600 + 1, 3600)
-n_edge_active = [tnet.num_active_events(t[i], t[i + 1])
-                 for i in range(len(t) - 1)]
+n_event_active = [tnet.num_active_events(None, t[1])]+[tnet.num_active_events(t[i], t[i + 1]) for i in range(1,len(t) - 1)]
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.plot(t[:-1], n_edge_active, marker='.')
+ax.plot(t[:-1], n_event_active, marker='.')
 
 ax.set_xticks(t)
 ax.set_xticklabels([i // 3600 for i in t], rotation=90)
