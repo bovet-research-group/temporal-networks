7e3576f51e83634160c20da737ee3152324ff4d7
Jonas I. Liechti <j-i-l@t4d.ch>
2026-08-02 22:51:28 +0200

    flag the missing save/load for laplacian_dynamics

--- tests/test_temporal_network.py
+++ tests/test_temporal_network.py
@@ -301,6 +301,15 @@ class TestTempNetwork:
             pd.testing.assert_series_equal(sn_et.ending_times,
                                            ln_et.ending_times)
 
+    def test_save_and_load_preserves_laplacian_dynamics(self):
+        network = self._get_instance(self.simple, use_df=True)
+        network.compute_laplacian_matrices(dynamics="heat")
+
+        network.save(self.simple.tmp_pkl.name)
+        loaded_network = ContTempNetwork.load(self.simple.tmp_pkl.name)
+
+        assert loaded_network.laplacian_dynamics == "heat"
+
     def test_import_data(self):
         """Make sure we can work with data with incomplete node lists
         """
@@ -700,4 +709,3 @@ class TestContTempNetworkEndingTimesRequired:
             ending_times=[],
         )
         assert net.num_events == 0
-
